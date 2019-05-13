5360fb28a58c2d001070c0c33e37a8b7195f4b7e
frederic jonquieres <fjonquieres83@gmail.com>
2019-05-13 16:48:06 +0100

    Restaurant app running need to finish front end

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # # Read all
+  # get "restaurants", to: "restaurants#index"
+  # # Read one
+  # get "restaurants/:id", to: "restaurant#show"
+  # #create a new one
+  # get "restaurants/new", to: "restaurants#new"
+  # post "restaurants", to: "restaurants#create"
+  # #Update
+  # get "restaurants/:id/edit", to: "restaurants#edit"
+  # patch "restaurants/:id", to: "restaurants#update"
+  # # Delete
+  # delete "restaurants/:id", to: "restaurants#destroy"
+  resources :restaurants do
+    resources :reviews, only: [:new, :create]
+  end
 end
